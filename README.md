c95c7d93d325d313fd1aafb4b182d19acbe6bbf7
Sahil Thapar <sahilthapar>
2023-03-29 15:16:37 -0700

    Leet Code 121: Buy High and Sell Low

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# word-mappers
+# Leet Code 75
+
+[Leet Code 1: Two Sum](two_sum.py)
+[Leet Code 121: Best Time to Buy and Sell Stocks]() 
---
+++ two_pointer.py
@@ -0,0 +1,15 @@
+def two_pointer(prices: list):
+    left, right = 0, 1
+    max_profit = 0
+
+    while right < len(prices):
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            max_profit = max(max_profit, profit)
+        else:
+            left = right
+        right += 1
+    return max_profit
+
+
+print(two_pointer([7, 1, 5, 4, 6, 5]))
