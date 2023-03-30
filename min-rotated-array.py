9fb5a21e07a6b3e2515c6c17662550adaab8fc75
Sahil Thapar <sahilthapar>
2023-03-30 15:54:21 -0700

    LeetCode 33: Minimum in Rotated Sorted Array

--- min-rotated-array.py
+++ min-rotated-array.py
@@ -4,24 +4,25 @@ def min_rotated_array(nums: list[int]) -> int:
     :param nums:
     :return:
     """
-
     # set a left and right pointer
-    l, r = 0, len(nums) - 1
+    left, right = 0, len(nums) - 1
 
     # if list is already sorted return the first element
-    if l == r or nums[l] < nums[r]:
-        return nums[l]
+    if left == right or nums[left] < nums[right]:
+        return nums[left]
 
     # find the middle index
-    m = (l+r) // 2
+    middle = (left + right) // 2
 
     # if the middle number is greater than the left pointer
     # it means that the left side is already sorted so search only the right side
-    if nums[m] >= nums[l]:
-        return min(nums[m], min_rotated_array(nums[m+1:]))
+    if nums[middle] >= nums[left]:
+        return min(nums[middle], min_rotated_array(nums[middle + 1:]))
 
     # otherwise the right side has the smallest number
-    return min(nums[m], min_rotated_array(nums[:m]))
+    return min(nums[middle], min_rotated_array(nums[:middle]))
 
-# x = min_rotated_array([2, 3, 1])
-# print(x)
+
+assert min_rotated_array([3, 4, 5, 1, 2]) == 1
+assert min_rotated_array([4, 5, 6, 7, 0, 1, 2]) == 0
+assert min_rotated_array([11, 13, 15, 17]) == 11
