cb8a12f8c5739e0b41b61eacd631de59ca31bff4
Sahil Thapar <sahilthapar>
2025-05-18 09:46:51 -0700

    Add method to check if a card can be played on top of another card

--- python/card-game/card.py
+++ python/card-game/card.py
@@ -1,3 +1,4 @@
+import typing
 from enum import Enum
 from typing import Optional
 
@@ -27,3 +28,40 @@ class Card:
         self.type = card_type
         self.color = color
         self.value = value
+
+    def is_wild(self):
+        """
+        Checks if card_type is wild
+        :return:
+        """
+        return self.type in [CardType.WILD, CardType.WILD_DRAW_FOUR]
+
+    def can_play_on_top_of(self, other_card: typing.Self, chosen_color: Optional[CardColor] = None) -> bool:
+        """
+        Checks if this card can be played on top of the "other_card"
+        - if wild, return true, can always be played
+        - if other_card is wild, just match chosen color
+        - else
+            - check if color matches
+        :param other_card:
+        :param chosen_color:
+        :return:
+        """
+        if self.is_wild():
+            return True
+
+        if other_card.is_wild() and self.color == chosen_color:
+            return True
+
+        if self.color == other_card.color:
+            return True
+
+        if self.type == other_card.type == CardType.NUMBER and self.value == other_card.value:
+            return True
+
+        if self.type == other_card.type != CardType.NUMBER:
+            return True
+
+        return False
+
+
